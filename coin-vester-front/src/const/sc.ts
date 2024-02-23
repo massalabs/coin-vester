dd0d5e26371f5b701cde9d96ddbf09122ca37e61
thomas-senechal <thomas.senechal@pm.me>
2024-02-23 16:39:17 +0100

    Add VESTING_SESSION_STORAGE_COST constant

--- coin-vester-front/src/const/sc.ts
+++ coin-vester-front/src/const/sc.ts
@@ -1,2 +1,5 @@
+import { fromMAS } from '@massalabs/massa-web3';
+
 export const SC_ADDRESS =
   'AS14oUcMujECzburUs8iMZZGsJQQQ61MxPVUKyV8WUNrw3mHmSqF';
+export const VESTING_SESSION_STORAGE_COST = fromMAS(2);
