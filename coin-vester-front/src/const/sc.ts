ad170abf0edcec146aa21cbebfd3cf6ea1e29d3f
Nathan Seva <thykof@protonmail.ch>
2024-02-28 22:10:21 +0100

    default fees in env var

--- coin-vester-front/src/const/sc.ts
+++ coin-vester-front/src/const/sc.ts
@@ -2,3 +2,4 @@ import { fromMAS } from '@massalabs/massa-web3';
 
 export const SC_ADDRESS = import.meta.env.VITE_SC_ADDRESS;
 export const VESTING_SESSION_STORAGE_COST = fromMAS(2);
+export const DEFAULT_OP_FEES = import.meta.env.VITE_OP_FEES;
