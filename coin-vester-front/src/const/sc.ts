9a3dd5c7ed0c920b9c2d353a5c8bf686b8fd5362
Nathan Seva <thykof@protonmail.ch>
2024-03-08 10:48:11 +0100

    add a log (#27)

--- coin-vester-front/src/const/sc.ts
+++ coin-vester-front/src/const/sc.ts
@@ -6,6 +6,7 @@ let defaultOpFees: bigint;
 try {
   defaultOpFees = BigInt(import.meta.env.VITE_OP_FEES);
 } catch (error) {
+  console.error('Failed to parse VITE_OP_FEES', error);
   defaultOpFees = BigInt(0);
 }
 export const DEFAULT_OP_FEES = defaultOpFees;
